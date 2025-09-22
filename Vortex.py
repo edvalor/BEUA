c96df2d3885fb11383fadee613ec432e3ec0dacd
edvalor <hugo.silva.ciee@valor.com.br>
2025-09-22 15:31:38 -0300

    Update Vortex.py

--- Vortex.py
+++ Vortex.py
@@ -1,6 +1,6 @@
 #BIBLIOTECAS
-import time
 import os
+import time
 from flask_cors import CORS
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
@@ -14,7 +14,199 @@ CORS(app)
 
 #DEFs DE RASPAGEM
 
-def close_indice():
+
+
+
+
+# Américas
+def DJI():
+    # Estados Unidos - América do Norte
+    pass
+
+def DJU():
+    # Estados Unidos - América do Norte
+    pass
+
+def SP100():
+    # Estados Unidos - América do Norte
+    pass
+
+def SP500():
+    # Estados Unidos - América do Norte
+    pass
+
+def NASDAQ():
+    # Estados Unidos - América do Norte
+    pass
+
+def NASDAQ100():
+    # Estados Unidos - América do Norte
+    pass
+
+def NBI():
+    # Estados Unidos - América do Norte
+    pass
+
+def BANK():
+    # Estados Unidos - América do Norte
+    pass
+
+def INDS():
+    # Estados Unidos - América do Norte
+    pass
+
+def INSR():
+    # Estados Unidos - América do Norte
+    pass
+
+def NQBTC():
+    # Estados Unidos - América do Norte
+    pass
+
+def NQETH():
+    # Estados Unidos - América do Norte
+    pass
+
+def NCI():
+    # Estados Unidos - América do Norte
+    pass
+
+def SPTSX():
+    # Canadá - América do Norte
+    pass
+
+def IPC():
+    # México - América do Norte
+    pass
+
+def IXCO():
+    # México - América do Norte
+    pass
+
+def IXTC():
+    # México - América do Norte
+    pass
+
+def MERVAL():
+    # Argentina - América do Sul
+    pass
+
+
+# Europa
+def FTSE100():
+    # Reino Unido - Europa
+    pass
+
+def NMX():
+    # Reino Unido - Europa
+    pass
+
+def DAX():
+    # Alemanha - Europa
+    pass
+
+def CAC40():
+    # França - Europa
+    pass
+
+def FTSEMIB():
+    # Itália - Europa
+    pass
+
+def IBC():
+    # Espanha - Europa
+    pass
+
+def PSI20():
+    # Portugal - Europa
+    pass
+
+def SMI():
+    # Suíça - Europa
+    pass
+
+def BEL20():
+    # Bélgica - Europa
+    pass
+
+def AMX():
+    # Holanda - Europa
+    pass
+
+def ASCX():
+    # Holanda - Europa
+    pass
+
+def AEX():
+    # Holanda - Europa
+    pass
+
+def OMXC20():
+    # Dinamarca - Europa
+    pass
+
+def OMXS30():
+    # Suécia - Europa
+    pass
+
+def WIG():
+    # Polônia - Europa
+    pass
+
+def EUROTOP100():
+    # Zona Euro - Europa
+    pass
+
+def N100():
+    # Zona Euro - Europa
+    pass
+
+def N150():
+    # Zona Euro - Europa
+    pass
+
+def MCX():
+    # Russia - Europa
+    pass
+
+# Ásia e Oceania
+def NIKKEI():
+    # Japão - Ásia
+    pass
+
+def SSE():
+    # China - Ásia
+    pass
+
+def SZSE():
+    # China - Ásia
+    pass
+
+def KOSPI():
+    # Coreia do Sul - Ásia
+    pass
+
+def TA125():
+    # Israel - Ásia
+    pass
+
+def XAO():
+    # Austrália - Oceania
+    pass
+
+
+# Criptomoedas
+def ETHUSD_RR():
+    # Global - Criptomoeda
+    pass
+
+def ETHUSD_RTI():
+    # Global - Criptomoeda
+    pass
+
+
+
+
 
 
 #MEIO (PARTE 1)
@@ -27,7 +219,51 @@ def home():
     return jsonify({
         "message": "API CLOSE",
         "endpoints": [
-            "/close/value"
+            "/close/DJI",
+            "/close/DJU",
+            "/close/SP100",
+            "/close/SP500",
+            "/close/NASDAQ",
+            "/close/NASDAQ100",
+            "/close/NBI",
+            "/close/BANK",
+            "/close/INDS",
+            "/close/INSR",
+            "/close/NQBTC",
+            "/close/NQETH",
+            "/close/NCI",
+            "/close/SPTSX",
+            "/close/IPC",
+            "/close/IXCO",
+            "/close/IXTC",
+            "/close/MERVAL",
+            "/close/FTSE100",
+            "/close/NMX",
+            "/close/DAX",
+            "/close/CAC40",
+            "/close/FTSEMIB",
+            "/close/IBC",
+            "/close/PSI20",
+            "/close/SMI",
+            "/close/BEL20",
+            "/close/AMX",
+            "/close/ASCX",
+            "/close/AEX",
+            "/close/OMXC20",
+            "/close/OMXS30",
+            "/close/WIG",
+            "/close/EUROTOP100",
+            "/close/N100",
+            "/close/N150",
+            "/close/MCX",
+            "/close/NIKKEI",
+            "/close/SSE",
+            "/close/SZSE",
+            "/close/KOSPI",
+            "/close/TA125",
+            "/close/XAO",
+            "/close/ETHUSD_RR",
+            "/close/ETHUSD_RTI",
         ]
     }),
 
@@ -37,15 +273,15 @@ def home():
 #AQUI EU COLOCO AS DEFs DE RETORNO
 
 
-@app.route('/close/indice', methods=['GET'])
-def api_close_indice():
-    resultado = close_indice()
+@app.route('/close/DJI', methods=['GET'])
+def api_close_DJI():
+    resultado = DJI()
     return jsonify(resultado)
 
 @app.route('/close/value', methods=['GET'])
 def api_value():
     return jsonify({
-        "Fechamento": close_indice()
+        "Fechamento": ()
     })
 
 #FIM
